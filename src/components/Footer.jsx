52f9bbfc875599cfa8f159db71d92fcc671711bb
sumeetgwork-stack <sumeetgwork@gmail.com>
2026-05-01 02:16:25 +0530

    Updated footer icons, size, and added Full Stack Developer role

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,13 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa'
 const Footer = () => (
   <footer className="footer">
     <div className="container">
-      <p>© 2025 Sumeet Gupta. All rights reserved.</p>
+      <div className="footer-left">
+        <p>© 2025 Sumeet Gupta. All rights reserved.</p>
+        <span className="footer-role">Full Stack Developer</span>
+      </div>
       <div className="footer-socials">
-        <a href="https://github.com/sumeetgwork-stack" target="_blank" rel="noreferrer"><FaGithub /></a>
-        <a href="https://www.linkedin.com/in/sumeetgupta07/" target="_blank" rel="noreferrer"><FaLinkedin /></a>
+        <a href="https://github.com/sumeetgwork-stack" target="_blank" rel="noreferrer" className="github"><FaGithub /></a>
+        <a href="https://www.linkedin.com/in/sumeetgupta07/" target="_blank" rel="noreferrer" className="linkedin"><FaLinkedin /></a>
       </div>
     </div>
   </footer>
